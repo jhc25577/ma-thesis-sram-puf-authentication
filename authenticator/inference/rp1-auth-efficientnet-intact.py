b988fce0768f91fddb98d5e965993913dbca75c5
changj <chang07@ads.uni-passau.de>
2024-02-23 15:21:28 +0100

    implementation for sending public key to client and base64 decryption for covering null and special bytes

--- authenticator/inference/rp1-auth-efficientnet-intact.py
+++ authenticator/inference/rp1-auth-efficientnet-intact.py
@@ -6,6 +6,7 @@ import argparse
 import ecies
 import tempfile
 from ecies.utils import generate_eth_key, generate_key
+import base64
 
 def server_program():
     # get the hostname/ip address
@@ -25,14 +26,16 @@ def server_program():
     SEPARATOR = "<SEPARATOR>"
     ENCODING = "utf-8"
 
-    # TODO: create a public and private key using ecies
-    # eth_k = generate_eth_key()
+    # create a public and private key using ecies
+    eth_k = generate_eth_key()
     # private key
-    # sk_hex = eth_k.to_hex()
-    # temporary private key
-    sk_hex = "0xc82be1019b06e83b2544461c3b3d91e8eb44462e1dcaea6b7441af648a61a3b7"
+    sk_hex = eth_k.to_hex()
+    # static private key
+    # sk_hex = "0xc82be1019b06e83b2544461c3b3d91e8eb44462e1dcaea6b7441af648a61a3b7"
     # public key
-    # pk_hex = eth_k.public.to_hex()
+    pk_hex = eth_k.public.to_hex()
+    print("Private key: ", sk_hex)
+    print("Public key: ", pk_hex)
 
     # configure how many client the server can listen simultaneously
     server_socket.listen(1)
@@ -71,8 +74,8 @@ def server_program():
     if board in devices:
         time.sleep(2)
         
-        # TODO: send public key
-        # conn.send(f"{pk_hex}".encode())
+        # send public key
+        conn.send(f"{pk_hex}".encode())
         # file related stuff
         conn.send("Please send the board image".encode(ENCODING))
         received = conn.recv(BUFFER_SIZE).decode()
@@ -82,7 +85,7 @@ def server_program():
         # informing the recived file 
         print("Receiving file:", filename)
 
-        # TODO: probably make a temp file and then decrypt it to make a real file
+        # store encrypted image as a byte array
         ecc = bytearray()
         with open(filename, "wb") as f:
             while True:
@@ -94,9 +97,11 @@ def server_program():
                     break
                 # write to the file the bytes we just received
                 ecc.extend(bytes_read)
-            # TODO: base64 decoding before/after decrypting
+            # base64 decoding before/after decrypting
             print("Decrypting...")
-            img = ecies.decrypt(sk_hex, bytes(ecc))
+            enc = base64.b64decode(bytes(ecc))
+            dec = ecies.decrypt(sk_hex, enc)
+            img = base64.b64decode(dec)
             f.write(img)
 
             
